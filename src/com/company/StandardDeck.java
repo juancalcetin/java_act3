244698494d0eb6bef8a86ee8851d0731fef9fdb1
juancalcetin <a>
2021-07-18 17:34:30 -0500

    primer commit

--- src/com/company/StandardDeck.java
+++ src/com/company/StandardDeck.java
@@ -8,12 +8,6 @@ public class StandardDeck {
 
     private Set<PlayingCard> deck = new LinkedHashSet<>();
 
-    /**
-     * Constructs an instance of StandardDeck, populating the internal data
-     * structure with 52 instances of the PlayingCard object, one for each
-     * combination of Rank and Suit defined
-     *
-     */
     public StandardDeck() {
         System.out.println("Las cartas en el deck son: ");
         // Populate deck with all 52 available PlayingCards
@@ -24,22 +18,13 @@ public class StandardDeck {
         }
     }
 
-    /**
-     * Takes the existing ordered set of PlayingCard objects and shuffles the
-     * order. The size of the deck remains the same.
-     *
-     */
     public void shuffle() {
         System.out.println("Se mezclo el Deck");
         List<PlayingCard> cardsInDeck = new LinkedList<>(deck);
         int totalDeckLength = cardsInDeck.size();
         Set<PlayingCard> newDeck = new LinkedHashSet<>();
-
-        // Iterate through the deck, randomly select a card, remove from the old
-        // deck and add to the new deck.
         for (int i = 0; i < totalDeckLength; i++) {
             PlayingCard randomlySelectedCard = cardsInDeck.get(rand.nextInt(cardsInDeck.size()));
-
             cardsInDeck.remove(randomlySelectedCard);
             newDeck.add(randomlySelectedCard);
         }
@@ -47,15 +32,6 @@ public class StandardDeck {
         deck = newDeck;
     }
 
-    /**
-     * Removes one card from the 'top' of the deck and returns it to the client.
-     * The size of the deck after the method completes is exactly one less than
-     * prior to calling the method. The remaining cards in the deck maintain
-     * their original order. When the deck no longer has any cards, the method
-     * returns null.
-     *
-     * @return PlayingCard Selected from the 'top' of the deck
-     */
     public PlayingCard deal_one_card() {
         if (deck.size() > 0) {
             List<PlayingCard> cardsInDeck = new LinkedList<>(deck);
@@ -102,11 +78,7 @@ public class StandardDeck {
         } return null;
 
     }
-    /**
-     * Utility method for retrieving the current state of the cards in the deck
-     *
-     * @return deck A set of the remaining PlayingCard objects in the deck
-     */
+
     public Set<PlayingCard> getDeck() {
         return deck;
     }
